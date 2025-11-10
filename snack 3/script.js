63face52643805995524929a33a0de98c0987c4a
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-11-10 16:09:59 +0100

    feat: snack 3

--- snack 3/script.js
+++ snack 3/script.js
@@ -4,3 +4,21 @@ un array e due numeri (a più piccolo di b).
 La funzione ritornerà un nuovo array con i valori 
 che hanno la posizione compresa tra i due numeri. 
 Non usare i metodi di array di JS */
+
+const numeri = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+const a = 3;
+const b = 8;
+
+function calcolo(numeri, a, b) {
+  const newArr = [];
+  for (let i = 0; i < numeri.length; i++) {
+    const curNum = numeri[i];
+    if (i > a && i < b) {
+      newArr.push(curNum);
+    }
+  }
+  return newArr;
+}
+
+const risultato = calcolo(numeri, a, b);
+console.log(risultato);
