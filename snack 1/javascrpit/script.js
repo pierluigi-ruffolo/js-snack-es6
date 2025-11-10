613a93ab246269707a14af3e4b03ae91f1c7c89e
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-11-10 14:44:29 +0100

    feat: completo snack 1, organizzo cartelle

--- snack 1/javascrpit/script.js
+++ snack 1/javascrpit/script.js
@@ -24,13 +24,3 @@ for (let i = 1; i < biciclette.length; i++) {
 }
 
 resultElement.innerHTML = `<p>la bici con peso minore è ${bici} con un peso di ${minBici}</p>`;
-//* Snack2
-/*Creare un array di oggetti di squadre di calcio.
-Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
-Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
-Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
-
-/* Snack 3 (Bonus)
-Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b). 
-La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri. Non usare i metodi di array di JS */
---
+++ snack 3/javascript.js/script.js
@@ -0,0 +1,6 @@
+/* Snack 3 (Bonus)
+Scrivere una funzione che accetti tre argomenti, 
+un array e due numeri (a più piccolo di b). 
+La funzione ritornerà un nuovo array con i valori 
+che hanno la posizione compresa tra i due numeri. 
+Non usare i metodi di array di JS */
